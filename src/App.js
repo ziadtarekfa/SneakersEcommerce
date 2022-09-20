30ca0ce74ec88de78604e5ff9d43aa663ce0617e
Ziad Tarek <ziadtarekfa@gmail.com>
2022-09-20 03:39:08 +0200

    added product component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import ProductCard from './components/ProductCard';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Footer />
+      <div className='container-products'>
+        <ProductCard />
+      </div>
     </div>
   );
 }
---
+++ src/components/ProductCard.js
@@ -0,0 +1,23 @@
+import ProductImage from '../assets/images/image-product-1.jpg';
+import '../styles/ProductCard.css';
+
+const ProductCard = () => {
+    return (
+        <div className="ProductCard">
+
+            <img src={ProductImage} alt='product' />
+            <h6>GIVENCHY</h6>
+            <h2>Urban Street Leather Sneakers</h2>
+            <div className='prices-container'>
+                <p style={{ 'font-weight': 'bold', }}>$450</p>
+                <p style={{ 'color': '#76787F', 'textDecoration': 'line-through' }}>$650</p>
+                <div className='discount-percentage-container'>
+                    <p>30%</p>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default ProductCard;
