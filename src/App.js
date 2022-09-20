9b3eeb7e0bac55b672a1725f262a577ba6d66910
Ziad Tarek <ziadtarekfa@gmail.com>
2022-09-20 16:51:55 +0200

    added brand card component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import ProductCard from './components/ProductCard';
 import headerImage from './assets/Headers/men-header.jpg';
+import BrandCard from './components/BrandCard';
 
 function App() {
   return (
     <div className="App">
       <Header />
 
-      <CategoryCard headerImage={headerImage} category="Men" />
+      {/* <CategoryCard headerImage={headerImage} category="Men" /> */}
+
       <Footer />
 
     </div>
---
+++ src/components/BrandCard.js
@@ -0,0 +1,12 @@
+import '../styles/BrandCard.css';
+import addidas from '../assets/collections/adidas-logo.svg';
+
+const BrandCard = ({ brandLogo, brandName }) => {
+    return (
+        <div className="brand-card">
+            <img src={brandLogo} alt={brandName} />
+        </div>
+    );
+}
+
+export default BrandCard;
