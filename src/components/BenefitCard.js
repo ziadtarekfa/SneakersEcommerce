cc95d0831c2ddbc50381842c327cb419f56dd874
Ziad Tarek <ziadtarekfa@gmail.com>
2022-09-20 02:24:43 +0200

    added BenefitsCard component

---
+++ src/components/BenefitCard.js
@@ -0,0 +1,18 @@
+import '../styles/BenefitCard.css';
+
+const BenefitCard = ({ image, alternateText, heading, paragraph }) => {
+
+    return (
+        <div className="BenefitCard">
+            <div className='image-container'>
+                <img src={image} alt={alternateText} />
+            </div>
+            <h1>{heading}</h1>
+            <p>
+                {paragraph}
+            </p>
+        </div>
+    );
+}
+
+export default BenefitCard;
