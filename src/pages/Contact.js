a7a2487b6bfc3d54722beef9115e1d746b4d3aa3
Ziad Tarek <ziadtarekfa@gmail.com>
2022-09-26 02:12:56 +0200

    feedback sent to firebase

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,36 +3,24 @@ import CategoryCard from '../components/CategoryCard';
 import { IoLocation, IoCall } from "react-icons/io5";
 import { MdEmail } from "react-icons/md";
 
-import { getDatabase, ref, child, push, update, set } from 'firebase/database';
+import { getDatabase, ref, push } from 'firebase/database';
 
 
 import '../pagesStyles/Contact.css';
 
 const Contact = () => {
 
-    // function submitFeedBackOrQuestions() {
-    //     const db = getDatabase();
+    function submitFeedBackOrQuestions(e) {
 
-    //     set(ref(db, 'contact/eu12'), {
-    //         name: document.getElementById('name'),
-    //         email: document.getElementById('email'),
-    //         title: document.getElementById('title'),
-    //         message: document.getElementById('message')
-    //     });
-    // const contactData = {
-    //     name: document.getElementById('name'),
-    //     email: document.getElementById('email'),
-    //     title: document.getElementById('title'),
-    //     message: document.getElementById('message')
-    // }
-    // const newContactKey = push(child(ref(db), 'contact')).key;
-    // const updates = {}
-    // updates['contact/' + newContactKey] = contactData;
-    // // updates['/user-posts/' + uid + '/' + newPostKey] = postData;
-
-    // update(ref(db), updates);
-    // return update(ref(db), updates);
-
+        e.preventDefault();
+        const db = getDatabase();
+        push(ref(db, `contact/`), {
+            name: document.getElementById('name').value,
+            email: document.getElementById('email').value,
+            title: document.getElementById('title').value,
+            message: document.getElementById('message').value
+        });
+    }
 
     return (
         <main>
@@ -56,15 +44,15 @@ const Contact = () => {
                     </div>
                 </section>
 
-                <section className='contact-form'>
+                <section className='contact-form' onSubmit={submitFeedBackOrQuestions} >
                     <form >
                         <input id="name" className='default-input' placeholder='Name' required></input>
                         <input id="email" className='default-input' placeholder='Email Address' type="email" required></input>
-                        <input id="title" className='default-input' placeholder='Title' required></input>
+                        <input id="title" className='default-input' placeholder='Title'></input>
                         <textarea id="message" className='default-input' placeholder='Message' required></textarea>
                         <button className='default-button'>Submit</button>
                     </form>
-                    {/* <button className='default-button'>Submit</button> */}
+
                 </section>
 
             </div>
